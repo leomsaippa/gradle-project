b6c17b317392c8c5295736a277159ea81f193fb3
leomsaippa <leonardo_saippa@id.uff.br>
2019-02-03 00:42:51 -0200

    Adds java library

---
+++ java-joker/src/main/java/com/lsaippa/java_joker/JavaJoker.java
@@ -0,0 +1,8 @@
+package com.lsaippa.java_joker;
+
+public class JavaJoker {
+
+    public String getJoker(){
+        return "Q: Why did the chewing gum cross the road? A: He was stuck to the chicken's foot.";
+    }
+}
