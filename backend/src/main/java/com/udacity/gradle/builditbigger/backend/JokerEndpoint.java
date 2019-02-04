ace689e2e88599fc8d0350cc45b3251e727c7ad3
leomsaippa <leonardo_saippa@id.uff.br>
2019-02-04 00:29:46 -0200

    Integrates with next screen and with GCM

---
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/JokerEndpoint.java
@@ -0,0 +1,38 @@
+package com.udacity.gradle.builditbigger.backend;
+
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.ApiNamespace;
+import com.lsaippa.java_joker.JavaJoker;
+
+/**
+ * An endpoint class we are exposing
+ */
+@Api(
+        name = "jokerApi",
+        version = "v1",
+        namespace = @ApiNamespace(
+                ownerDomain = "backend.builditbigger.gradle.udacity.com",
+                ownerName = "backend.builditbigger.gradle.udacity.com",
+                packagePath = ""
+        )
+)
+public class JokerEndpoint {
+
+    private JavaJoker joker = new JavaJoker();
+
+    /**
+     * A simple endpoint method that takes a name and says Hi back
+     */
+    @ApiMethod(
+            name = "tellJoke",
+            httpMethod = ApiMethod.HttpMethod.GET)
+    public MyBean tellJoke() {
+        MyBean response = new MyBean();
+        response.setData(joker.getJoker());
+
+        return response;
+    }
+
+
+}
